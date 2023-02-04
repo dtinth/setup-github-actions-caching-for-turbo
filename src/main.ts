5b7318c8046999e5c1c4c713efd651b755e14663
dtinth on MBP M1 <dtinth@spacet.me>
2023-02-04 19:23:26 +0700

    No waiting!

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,11 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
 import {inspect} from 'util'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    // const ms: string = core.getInput('milliseconds')
     core.info(inspect(process.argv))
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
     core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
